2de4f979db3c9c2ed4c9fd10ec7f0e9d05eac73c
Laharisrikotipalli <laharisrikotipalli07@gmail.com>
2026-01-19 21:19:32 +0530

    Fix Lambda function_name error (remove prefix, add random suffix)

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,9 +1,22 @@
+########################################
+# Zip Lambda source
+########################################
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = var.source_path
   output_path = "${path.root}/.terraform-lambda/${var.function_name}.zip"
 }
 
+########################################
+# Random suffix (Lambda names must be unique)
+########################################
+resource "random_id" "lambda_suffix" {
+  byte_length = 4
+}
+
+########################################
+# IAM Role for Lambda
+########################################
 resource "aws_iam_role" "lambda_role" {
   name_prefix = "${var.function_name}-role-"
 
@@ -21,13 +34,19 @@ resource "aws_iam_role" "lambda_role" {
   tags = var.tags
 }
 
-
+########################################
+# Attach basic execution policy
+########################################
 resource "aws_iam_role_policy_attachment" "basic_execution" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+########################################
+# Lambda Function (REQUIRED function_name)
+########################################
 resource "aws_lambda_function" "this" {
-  function_name_prefix = "${var.function_name}-"
+  function_name = "${var.function_name}-${random_id.lambda_suffix.hex}"
 
   role    = aws_iam_role.lambda_role.arn
   handler = var.handler
@@ -48,3 +67,22 @@ resource "aws_lambda_function" "this" {
   tags = var.tags
 }
 
+########################################
+# CloudWatch Alarm
+########################################
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  alarm_name          = "${aws_lambda_function.this.function_name}-errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 0
+
+  dimensions = {
+    FunctionName = aws_lambda_function.this.function_name
+  }
+
+  tags = var.tags
+}
